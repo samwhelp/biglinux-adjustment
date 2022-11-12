25289b01275689a9f97e37f36692983d056e6a0b
samwhelp <samwhelp@users.noreply.github.com>
2022-11-12 21:47:50 +0800

    fix

--- prototype/keybind/kdebiglinux/modern/config-install.sh
+++ prototype/keybind/kdebiglinux/modern/config-install.sh
@@ -8,21 +8,100 @@ set -e
 ##
 kde_config_install () {
 
-	echo "## Config: kde"
+	kde_global_shortcuts_server_stop
+	kde_config_install_kde_keybind
+	kde_global_shortcuts_server_start
+
+}
+##
+### Tail: kde
+################################################################################
+
+
+################################################################################
+### Head: kde / keybind
+##
+kde_config_install_kde_keybind () {
+
+	echo
+	echo "##"
+	echo "## Config: kde_config_install_kde_keybind"
+	echo "##"
 	echo
 
-	echo "mkdir -p $HOME/.config/"
-	mkdir -p "$HOME/.config/"
+
+	echo "mkdir -p ${HOME}/.config/"
+	mkdir -p "${HOME}/.config/"
 
 
-	echo "install -Dm644 ./kglobalshortcutsrc $HOME/.config/kglobalshortcutsrc"
-	install -Dm644 "./kglobalshortcutsrc" "$HOME/.config/kglobalshortcutsrc"
+	echo "install -Dm644 ./kglobalshortcutsrc ${HOME}/.config/kglobalshortcutsrc"
+	install -Dm644 "./kglobalshortcutsrc" "${HOME}/.config/kglobalshortcutsrc"
 
 
 	echo
 }
+
 ##
-### Tail: kde
+### Tail: kde / keybind
+################################################################################
+
+
+################################################################################
+### Head: kde / global shortcuts server
+##
+kde_global_shortcuts_server_stop () {
+
+	echo
+	echo "##"
+	echo "## Prepare: kde_global_shortcuts_server_stop"
+	echo "##"
+	echo
+
+
+	echo "systemctl --user stop plasma-kglobalaccel.service"
+	systemctl --user stop plasma-kglobalaccel.service
+
+
+	echo
+}
+
+kde_global_shortcuts_server_start () {
+
+	echo
+	echo "##"
+	echo "## Prepare: kde_global_shortcuts_server_start"
+	echo "##"
+	echo
+
+
+	echo "systemctl --user start plasma-kglobalaccel.service"
+	systemctl --user start plasma-kglobalaccel.service
+
+
+	echo
+}
+
+kde_global_shortcuts_server_restart () {
+	#kde_global_shortcuts_server_stop
+	#kde_global_shortcuts_server_start
+
+
+	echo
+	echo "##"
+	echo "## Prepare: kde_global_shortcuts_server_stop"
+	echo "##"
+	echo
+
+
+	echo "systemctl --user resart plasma-kglobalaccel.service"
+	systemctl --user resart plasma-kglobalaccel.service
+
+
+	echo
+
+}
+##
+### Tail: kde / global shortcuts server
 ################################################################################
 
 
